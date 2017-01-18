1963e12e07a267639e59ee54370a4f1fd7d48cb4
Sharlak <elanorigby@gmail.com>
2017-01-18 12:08:05 +0000

    added docstring

--- PEPSoup.py
+++ PEPSoup.py
@@ -2,9 +2,12 @@ import bs4 as bs
 import urllib.request
 
 def get_PEP_set():
+    """
+    scrapes all urls with the pep prefix from the pep index page and returns them as a set
+    :return:
+    """
     sauce = urllib.request.urlopen('https://www.python.org/dev/peps/').read()
     soup = bs.BeautifulSoup(sauce, 'lxml')
     peps = {url.get('href') for url in soup.find_all('a') if "/dev/peps/pep-" in url.get('href')}
 
-    # print(peps)
     return peps
