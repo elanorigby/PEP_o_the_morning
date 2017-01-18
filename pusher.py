93c0b6b32a97d2dc22eb18f619dff2569f9153f0
Sharlak <elanorigby@gmail.com>
2017-01-18 12:04:52 +0000

    updated main fucntion name

--- pusher.py
+++ pusher.py
@@ -1,5 +1,5 @@
 from pushbullet import Pushbullet
-from main import get_push_pep
+from PEP_prep import get_push_pep
 import APIkeys
 
 #TODO check for pusbullet.py upgrade
@@ -9,7 +9,5 @@ pb = Pushbullet(APIkeys.pushbullet)
 pepo_chan = pb.channels[0]
 
 push_pep = get_push_pep()
-# print(type(push_pep))
-# print(push_pep)
 
 push = pepo_chan.push_link("PEP o' the morning to ya!", push_pep)
