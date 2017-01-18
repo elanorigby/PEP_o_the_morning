7060976f7ad496a8b3a3e8e58c44d8abdd5afe1a
Sharlak <elanorigby@gmail.com>
2017-01-18 12:11:19 +0000

    added docstrings

--- pickle_town.py
+++ pickle_town.py
@@ -1,10 +1,21 @@
 import pickle
 
 def pickle_it(file, to_write):
+    """
+    replaces old data in pickle file with new data
+    :param file:
+    :param to_write:
+    :return:
+    """
     with open(file, 'wb') as f:
         pickle.dump(to_write, f)
 
 
 def read_pickle(file):
+    """
+    gets useable python collection out of pickle file
+    :param file:
+    :return:
+    """
     with open(file, 'rb') as f:
         return pickle.load(f)
