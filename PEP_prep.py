187d64883e1499b94b7188a2f6505f1c89a18efc
Sharlak <elanorigby@gmail.com>
2017-01-19 20:05:30 +0000

    total refactor

--- PEP_prep.py
+++ PEP_prep.py
@@ -1,35 +1,32 @@
-from PEPSoup import get_PEP_set
+from PEPSoup import get_fresh_peps
 from pickle_town import pickle_it, read_pickle
 import random
 
 
-def get_push_pep():
-    peps = get_PEP_set()
-    ref_pickle = 'ref.pickle'
-    use_pickle = 'use.pickle'
+def make_push_pep():
+    # scrape for current peps
+    fresh_peps = get_fresh_peps()
 
-    # read ref and use pickles into vars ref and use
-    ref_set = read_pickle(ref_pickle)
-    use_set = read_pickle(use_pickle)
+    # get used_peps list (from pickle)
+    used_peps = read_pickle('used.pickle')
 
-    if not use_set:
-        pickle_it(use_pickle, peps)
-        use_set = read_pickle(use_pickle)
+    # make sure that there are some unused peps, if not reset used list to empty
+    if used_peps == fresh_peps:
+        used_peps = []
 
-    # compare against ref file
-    if ref_set < peps:
-        # if difference, add new items to ref and use files
-        new_peps = peps.difference(ref_set)
-        ref_set.update(new_peps)
-        use_set.update(new_peps)
+    def get_pepo():
+        pepo = random.choice(fresh_peps)
+        if pepo in used_peps:
+            get_pepo()
+        else:
+            used_peps.append(pepo)
+            return pepo
 
-    # get random pep from use set
-    pepo = random.choice(list(use_set))
+    pepo = get_pepo()
 
-    # remove that pep from use set
-    use_set.remove(pepo)
+    pickle_it('used.pickle', used_peps)
 
-    # attach pep to rest of url
     push_pep = 'https://www.python.org' + pepo
+
     return push_pep
 
