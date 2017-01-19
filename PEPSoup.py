126e26ed219eeb132f909950ff94f0f7c0341537
Sharlak <elanorigby@gmail.com>
2017-01-19 18:16:07 +0000

    returns scraped peps as a list now

--- PEPSoup.py
+++ PEPSoup.py
@@ -9,6 +9,6 @@ def get_PEP_set():
     """
     sauce = urllib.request.urlopen('https://www.python.org/dev/peps/').read()
     soup = bs.BeautifulSoup(sauce, 'lxml')
-    peps = {url.get('href') for url in soup.find_all('a') if "/dev/peps/pep-" in url.get('href')}
+    peps = [url.get('href') for url in soup.find_all('a') if "/dev/peps/pep-" in url.get('href')]
 
     return peps
