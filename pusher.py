0ea987166f50c6a85c0299740331c8f44b7d12da
Sharlak <elanorigby@gmail.com>
2017-02-01 16:21:30 +0000

    added function to update flask app when pusher is run

--- pusher.py
+++ pusher.py
@@ -1,6 +1,7 @@
 from pushbullet import Pushbullet
-from PEP_prep import make_push_pep
+from PEP_prep import make_push_pep, pickle_it
 from secrets import APIkeys
+from flask_app import app_update
 
 #TODO check for pusbullet.py upgrade
 
@@ -15,3 +16,6 @@ push_pep = make_push_pep()
 
 # make push
 push = pepo_chan.push_link("PEP o' the morning to ya!", push_pep)
+
+# update flask app with current pep
+app_update(push_pep)
