1ac5e85493222000aab0e206f9e04167899e50ad
Sharlak <elanorigby@gmail.com>
2017-02-01 16:19:26 +0000

    first draft of flask app/ static webpage

--- views/flask_app.py
+++ views/flask_app.py
@@ -1,3 +1,13 @@
 from flask import Flask
+from flask import render_template
 
-# create app instance, named whatever current namespace is
+def app_update(pepo):
+    # create app instance named whatever current namespace is
+    app = Flask(__name__)
+
+    # wrap function in a route to make it a view
+    @app.route('/')
+    def index(pepo=pepo):
+        return render_template('main.html', pepo=pepo)
+
+    app.run(debug=False)
