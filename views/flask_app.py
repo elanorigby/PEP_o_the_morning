23b2e8cd57a94d768e5b0d6ab215f7cf2aa28295
Sharlak <elanorigby@gmail.com>
2017-02-02 17:55:28 +0000

    plugged in random color generator for the background

--- views/flask_app.py
+++ views/flask_app.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from flask import render_template
+import randomcolor
+
 
 def app_update(pepo, num):
     # create app instance named whatever current namespace is
     app = Flask(__name__)
 
+    rand_color = randomcolor.RandomColor()
+    color = str(rand_color.generate(luminosity='light')[0])
+    print(color)
+
     # wrap function in a route to make it a view
     @app.route('/')
-    def index(pepo=pepo):
-        return render_template('main.html', pepo=pepo, num=num)
+    def index(color=color):
+        return render_template('main.html', pepo=pepo, num=num, color=color)
 
     app.run(debug=True)
