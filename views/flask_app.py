330d350fec8514ed04d548356c3c27eabf3f26f1
Sharlak <elanorigby@gmail.com>
2017-02-07 16:41:20 +0000

    debug: having debug set to True made it call the function twice. Moved random color generation into its own function.

--- views/flask_app.py
+++ views/flask_app.py
@@ -3,17 +3,21 @@ from flask import render_template
 import randomcolor
 
 
+def get_color():
+    rand_color = randomcolor.RandomColor()
+    color = str(rand_color.generate(luminosity='light')[0])
+    print(color)
+    return color
+
 def app_update(pepo, num):
     # create app instance named whatever current namespace is
     app = Flask(__name__)
 
-    rand_color = randomcolor.RandomColor()
-    color = str(rand_color.generate(luminosity='light')[0])
-    print(color)
+    color = get_color()
 
     # wrap function in a route to make it a view
     @app.route('/')
     def index(color=color):
         return render_template('main.html', pepo=pepo, num=num, color=color)
 
-    app.run(debug=True)
+    app.run(debug=False)
