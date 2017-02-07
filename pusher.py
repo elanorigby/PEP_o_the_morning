c72b5b91d5ac9d01b78e3e1916245e3318d23079
Sharlak <elanorigby@gmail.com>
2017-02-07 16:32:37 +0000

    added comments

--- pusher.py
+++ pusher.py
@@ -5,7 +5,6 @@ from PEP_prep import make_push_pep
 from flask_app import app_update
 from PEP_info import pep_num
 
-
 #TODO check for pusbullet.py upgrade
 
 # identify self to pushbullet api
@@ -17,10 +16,10 @@ pepo_chan = pb.channels[0]
 # grab a pep to push out
 push_pep = make_push_pep()
 
-# make push
-# push = pepo_chan.push_link("PEP o' the morning to ya!", push_pep)
+# push it out
+push = pepo_chan.push_link("PEP o' the morning to ya!", push_pep)
 
-#get pep number
+# get pep number
 num = pep_num(push_pep)
 
 # update flask app with current pep
