6c9fa14e82e2179ac1dbe37b9bd206516bc1e87b
Sharlak <elanorigby@gmail.com>
2017-02-09 14:37:33 +0000

    replaced number will full name of pep

--- views/flask_app.py
+++ views/flask_app.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from flask import render_template
 import randomcolor
-
+from PEPSoup import get_pep_name
 
 def get_color():
     rand_color = randomcolor.RandomColor()
     color = str(rand_color.generate(luminosity='light')[0])
-    print(color)
     return color
 
 def app_update(pepo, num):
@@ -15,9 +14,11 @@ def app_update(pepo, num):
 
     color = get_color()
 
+    pep_name = get_pep_name(pepo)
+
     # wrap function in a route to make it a view
     @app.route('/')
     def index(color=color):
-        return render_template('main.html', pepo=pepo, num=num, color=color)
+        return render_template('main.html', pepo=pepo, pep_name=pep_name, color=color)
 
     app.run(debug=False)
