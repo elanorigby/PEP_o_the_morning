f37660d938ef18f42cb97bdb6f75f3e5ffc3f79a
Sharlak <elanorigby@gmail.com>
2017-02-09 15:00:44 +0000

    removed pep num logic

--- pusher.py
+++ pusher.py
@@ -3,7 +3,7 @@ from secrets import APIkeys
 
 from PEP_prep import make_push_pep
 from flask_app import app_update
-from PEP_info import pep_num
+
 
 #TODO check for pusbullet.py upgrade
 
@@ -19,8 +19,5 @@ push_pep = make_push_pep()
 # push it out
 push = pepo_chan.push_link("PEP o' the morning to ya!", push_pep)
 
-# get pep number
-num = pep_num(push_pep)
-
 # update flask app with current pep
-app_update(push_pep, num)
+app_update(push_pep)
