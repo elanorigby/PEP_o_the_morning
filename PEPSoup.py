89d5b03c70e62c7a681dbfb616cdc0ae1995e100
Sharlak <elanorigby@gmail.com>
2017-02-09 14:36:12 +0000

    created bs4 function to scrape name of today's pep from its webpage

--- PEPSoup.py
+++ PEPSoup.py
@@ -12,3 +12,11 @@ def get_fresh_peps():
     peps = [url.get('href') for url in soup.find_all('a') if "/dev/peps/pep-" in url.get('href')]
 
     return peps
+
+
+
+def get_pep_name(push_pep):
+    """ scrape by class=" page-title """
+    sauce = urllib.request.urlopen(push_pep).read()
+    soup = bs.BeautifulSoup(sauce, 'lxml')
+    return soup.find(class_='page-title').text
