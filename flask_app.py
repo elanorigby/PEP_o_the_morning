94dc1fc6bfa44df69f84462afa5babb41d9abff9
sharla <elanorigby@gmail.com>
2017-02-10 16:16:52 +0000

    changed logic around so that flask_app is the script that makes everything happen instead of pusher.

--- flask_app.py
+++ flask_app.py
@@ -1,28 +1,20 @@
 from flask import Flask
 from flask import render_template
-import randomcolor
 
+from color_get import get_color
+from pusher import push_it_real
 from PEPSoup import get_pep_name
 
+# create app instance named whatever current namespace is
+app = Flask(__name__)
 
-def get_color(lum):
-    rand_color = randomcolor.RandomColor()
-    color = str(rand_color.generate(luminosity=lum)[0])
-    return color
+bkcolor = get_color('light')
+hovcolor = get_color('dark')
 
+pepo = push_it_real()
+pep_name = get_pep_name(pepo)
 
-def app_update(pepo):
-    # create app instance named whatever current namespace is
-    app = Flask(__name__)
-
-    bkcolor = get_color('light')
-    hovcolor = get_color('dark')
-
-    pep_name = get_pep_name(pepo)
-
-    # wrap function in a route to make it a view
-    @app.route('/')
-    def index():
-        return render_template('main.html', pepo=pepo, pep_name=pep_name, bkcolor=bkcolor, hovcolor=hovcolor)
-
-    app.run(debug=False)
+# wrap function in a route to make it a view
+@app.route('/')
+def index():
+    return render_template('main.html', pepo=pepo, pep_name=pep_name, bkcolor=bkcolor, hovcolor=hovcolor)
--- pusher.py
+++ pusher.py
@@ -2,23 +2,21 @@ from pushbullet import Pushbullet
 
 from secrets import APIkeys
 from PEP_prep import make_push_pep
-from flask_app import app_update
 
+def push_it_real():
+    # identify self to pushbullet api
+    pb = Pushbullet(APIkeys.pushapikey)
 
-# identify self to pushbullet api
-pb = Pushbullet(APIkeys.pushapikey)
+    # set channel to push to
+    pepo_chan = pb.channels[0]
 
-# set channel to push to
-pepo_chan = pb.channels[0]
+    # grab a pep to push out
+    push_pep = make_push_pep()
 
-# grab a pep to push out
-push_pep = make_push_pep()
+    # push it out
+    push = pepo_chan.push_link("PEP o' the morning to ya!", push_pep)
 
-# push it out
-push = pepo_chan.push_link("PEP o' the morning to ya!", push_pep)
-
-# update flask app with current pep
-app_update(push_pep)
+    return push_pep
 
 
 
