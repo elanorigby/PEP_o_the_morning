fd34f9cd5b92dfd2eaff41775495c162127b2f61
Sharlak <elanorigby@gmail.com>
2017-08-01 12:16:54 +0100

    avoiding underscores in module names

--- views/flaskapp.py
+++ views/flaskapp.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask import render_template
 import randomcolor
 
-from PEPSoup import get_pep_name
+import scrape
 
 
-def get_color(lum):
+def color(lum):
     rand_color = randomcolor.RandomColor()
     color = str(rand_color.generate(luminosity=lum)[0])
     return color
@@ -15,14 +15,14 @@ def app_update(pepo):
     # create app instance named whatever current namespace is
     app = Flask(__name__)
 
-    bkcolor = get_color('light')
-    hovcolor = get_color('dark')
+    bkcolor = color('light')
+    hovcolor = color('dark')
 
-    pep_name = get_pep_name(pepo)
+    title = scrape.title(pepo)
 
     # wrap function in a route to make it a view
     @app.route('/')
     def index():
-        return render_template('main.html', pepo=pepo, pep_name=pep_name, bkcolor=bkcolor, hovcolor=hovcolor)
+        return render_template('main.html', pepo=pepo, title=title, bkcolor=bkcolor, hovcolor=hovcolor)
 
     app.run(debug=False)
