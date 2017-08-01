0a89e6328f346c119594eedd617e94beb238d3ed
Sharlak <elanorigby@gmail.com>
2017-08-01 12:22:34 +0100

    align with module naming conventions

--- push.py
+++ push.py
@@ -1,24 +1,24 @@
 from pushbullet import Pushbullet
 
 from secrets import APIkeys
-from PEP_prep import make_push_pep
-from flask_app import app_update
+from prepare import make_push_pep
+import flaskapp
 
 
 # identify self to pushbullet api
 pb = Pushbullet(APIkeys.pushapikey)
 
 # set channel to push to
-pepo_chan = pb.channels[0]
+channel = pb.channels[0]
 
 # grab a pep to push out
 push_pep = make_push_pep()
 
 # push it out
-push = pepo_chan.push_link("PEP o' the morning to ya!", push_pep)
+push = channel.push_link("PEP o' the morning to ya!", push_pep)
 
 # update flask app with current pep
-app_update(push_pep)
+flaskapp.update(push_pep)
 
 
 
