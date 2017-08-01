02ba0486143516beb73e4ea13c76cb1e25857a22
Sharlak <elanorigby@gmail.com>
2017-08-01 12:22:06 +0100

    align with module naming conventions"

--- prepare.py
+++ prepare.py
@@ -1,6 +1,9 @@
-from PEPSoup import get_fresh_peps
-from pickle_town import pickle_it, read_pickle
+# external
 import random
+
+# internal
+import scrape
+import pickler
 
 
 def get_pepo(fresh_peps, used_peps):
@@ -15,10 +18,10 @@ def get_pepo(fresh_peps, used_peps):
 
 def make_push_pep(debug=True):
     # scrape for current peps
-    fresh_peps = get_fresh_peps()
+    fresh_peps = scrape.peps()
 
     # get used_peps list (from pickle)
-    used_peps = read_pickle('used.p')
+    used_peps = pickler.read('used.p')
 
     # make sure that there are some unused peps, if not reset used list to empty
     if used_peps == fresh_peps:
@@ -27,7 +30,7 @@ def make_push_pep(debug=True):
     pepo = get_pepo(fresh_peps, used_peps)
 
     # put away new list of used peps
-    pickle_it('used.p', used_peps)
+    pickler.pickleit('used.p', used_peps)
 
     # add rest of url to pep
     push_pep = 'https://www.python.org' + pepo
